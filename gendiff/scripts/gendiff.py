c2fdc74dba56213e35aab523c928b80157f7899b
Eagle <eagle@Krapd-PC-001.hcsds.kem>
2024-08-12 15:07:15 +0700

    step 3 revision second

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
-
+from  gendiff.scripts.search_diff import generate_diff
 import argparse
+import json
 
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+def parse():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 
-parser.add_argument(metavar='first_file', dest='first_file')
-parser.add_argument(metavar='second_file', dest='second_file')
-parser.add_argument("-f", "--format", metavar="FORMAT",
-                    help="set format of output")
+    parser.add_argument(metavar='first_file', dest='first_file')
+    parser.add_argument(metavar='second_file', dest='second_file')
+    parser.add_argument("-f", "--format", metavar="FORMAT",
+                        help="set format of output")
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+    return parser.parse_args()
+#print(args.accumulate(args.integers))
+
+
+
+def main():
+    args = parse()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == '__main__':
+    main()
--- gendiff/scripts/search_diff.py
+++ gendiff/scripts/search_diff.py
@@ -1,40 +1,47 @@
 #from gendiff import parser
 import json
-import ast
 
 
 file1 = json.load(open('gendiff/scripts/file1.json'))
 file2 = json.load(open('gendiff/scripts/file2.json'))
 
+def get_normalise_string(string):
+    if isinstance(string, bool):
+        return str(string).lower()
+    elif isinstance(string, str):
+        return string.strip(' " " ')
+    elif isinstance(string, int):
+        return str(string)
+    else:
+        return string
 
-def print_dictionary_row_by_row(dictionary):
-    for key, value in dictionary.items():
-        return f"{key}: {value}"
 
 
 def generate_diff(file_one, file_two):
-    
+    result = ''
     differences = {}
     for key, value in file_one.items():
         if key not in file_two:
             key = f'- {key}'
-            differences[key] = value
+            differences[key] = get_normalise_string(value)
         elif value == file_two[key]:
             key = f'  {key}'
-            differences[key] = value
+            differences[key] = get_normalise_string(value)
         elif value != file_two[key]:
             key1 = f'- {key}'
-            differences[key1] = value
+            differences[key1] = get_normalise_string(value)
             key2 = f'+ {key}'
             differences[key2] = file_two.get(key)
     for key, value in file_two.items():
         if key not in file_one:
             key = f'+ {key}'
-            differences[key] = value
-    differences = ast.literal_eval(str(differences).replace('"', "'"))
-
-
-    return differences
-
-    #return print_dictionary_row_by_row(differences)
+            differences[key] = get_normalise_string(value)
+    differenc = dict(sorted(differences.items(), key=lambda x: x[0][2]))
+    for key, value in differenc.items():
+        sub_result = f'\n{key}: {value}'
+        result += sub_result
+    return f'{{{result}\n}}'
+    
 print(generate_diff(file1, file2))
+
+
