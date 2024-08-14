88526914e8e894a8cd8e0916ca5c7bb89836714b
Eagle <eagle@Krapd-PC-001.hcsds.kem>
2024-08-14 11:15:23 +0700

    step 3 revision for

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,13 +9,14 @@ def parse():
     parser.add_argument(metavar='first_file', dest='first_file')
     parser.add_argument(metavar='second_file', dest='second_file')
     parser.add_argument("-f", "--format", metavar="FORMAT",
-                        help="set format of output")
+                    help="set format of output")
 
     return parser.parse_args()
 #print(args.accumulate(args.integers))
 
 
-
+file1 = json.load(open('gendiff/scripts/file1.json'))
+file2 = json.load(open('gendiff/scripts/file2.json'))
 def main():
     args = parse()
     print(generate_diff(args.first_file, args.second_file))
--- gendiff/scripts/search_diff.py
+++ gendiff/scripts/search_diff.py
@@ -2,8 +2,8 @@
 import json
 
 
-file1 = json.load(open('gendiff/scripts/file1.json'))
-file2 = json.load(open('gendiff/scripts/file2.json'))
+#file1 = json.load(open('gendiff/scripts/file1.json'))
+#file2 = json.load(open('gendiff/scripts/file2.json'))
 
 def get_normalise_string(string):
     if isinstance(string, bool):
@@ -20,28 +20,50 @@ def get_normalise_string(string):
 def generate_diff(file_one, file_two):
     result = ''
     differences = {}
-    for key, value in file_one.items():
-        if key not in file_two:
-            key = f'- {key}'
-            differences[key] = get_normalise_string(value)
-        elif value == file_two[key]:
-            key = f'  {key}'
-            differences[key] = get_normalise_string(value)
-        elif value != file_two[key]:
-            key1 = f'- {key}'
-            differences[key1] = get_normalise_string(value)
-            key2 = f'+ {key}'
-            differences[key2] = file_two.get(key)
-    for key, value in file_two.items():
-        if key not in file_one:
-            key = f'+ {key}'
-            differences[key] = get_normalise_string(value)
-    differenc = dict(sorted(differences.items(), key=lambda x: x[0][2]))
-    for key, value in differenc.items():
-        sub_result = f'\n{key}: {value}'
+    if isinstance(file_one, dict):
+        for key, val in file_one.items():
+            if key not in file_two:
+                key = f'  - {key}'
+                differences[key] = get_normalise_string(val)
+            elif val == file_two[key]:
+                key = f'    {key}'
+                differences[key] = get_normalise_string(val)
+            elif val != file_two[key]:
+                key1 = f'  - {key}'
+                differences[key1] = get_normalise_string(val)
+                key2 = f'  + {key}'
+                differences[key2] = file_two.get(key)
+    if isinstance(file_two, dict):
+        for key, val in file_two.items():
+            if key not in file_one:
+                key = f'  + {key}'
+                differences[key] = get_normalise_string(val)
+    differenc = dict(sorted(differences.items(), key=lambda x: x[0][4]))
+    for key, val in differenc.items():
+        sub_result = f'\n{key}: {val}'
         result += sub_result
     return f'{{{result}\n}}'
-    
-print(generate_diff(file1, file2))
+
+
+'''def generate_diff(file_one, file_two):
+    def iter_(current_value, depth):
+        if not isinstance(current_value, dict):
+            return str(current_value)
+
+        deep_indent_size = depth + spaces_count
+        deep_indent = replacer * deep_indent_size
+        current_indent = replacer * depth
+        lines = []
+        for key, val in current_value.items():
+            lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
+        result = itertools.chain("{", lines, [current_indent + "}"])
+        return '\n'.join(result)
+
+    return iter_(value, 0)'''
+
+
+#print(generate_diff(file1, file2))
+
+
 
 
